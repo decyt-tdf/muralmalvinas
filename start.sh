22de0d0dd0a6d6d967f6fc50d340e002784c4a0b
Santiago Gil <gil.educaciontdf@gmail.com>
2017-08-03 09:58:42 -0300

    Corrige y nombra volumenes

--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@ if [ -z "`docker network ls | grep 'panel-net'`" ];
  then docker network create --driver bridge panel-net;
  fi
 docker run -d --name muraldb \
- -v /data/db \
+ -v mongo:/data/db \
  --network panel-net \
  -d mongo:3.4 
 docker run -d -p 8081 \
  --network panel-net \
  --name muralmalvinas \
- -v /home/img \
- -v /home/imgthumb \
+ -v img:/muralmalvinas/public/img \
+ -v imgthumb:/muralmalvinas/public/imgthumb \
  -e "VIRTUAL_HOST=muralmalvinas.tierradelfuego.gob.ar" \
  -e "HTTPS_METHOD=nohttps" \
  -e "VIRTUAL_PORT=8081" \
